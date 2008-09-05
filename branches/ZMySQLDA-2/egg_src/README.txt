accc89734fd683ce6ed44f459054489b14ffcaac
eikenberry <eikenberry@9fc6cd9a-920d-0410-adcf-ac96716ed7e8>
2008-09-05 01:25:12 +0000

    Egg packaging support files provided by Brett Carter.
    git-svn-id: https://mysql-python.svn.sourceforge.net/svnroot/mysql-python@547 9fc6cd9a-920d-0410-adcf-ac96716ed7e8

---
+++ branches/ZMySQLDA-2/egg_src/README.txt
@@ -0,0 +1,8 @@
+ZMySQLDA: Zope Mysql adatper product.
+
+To create egg package run something along the lines of:
+
+$ python setup.py egg_info -RDb "" sdist bdist_egg
+
+See setuptool documentation for more.
+http://peak.telecommunity.com/DevCenter/setuptools#making-official-non-snapshot-releases
---
+++ branches/ZMySQLDA-2/egg_src/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2.4
+from setuptools import setup, find_packages
+
+# See MANIFEST.in for filelist
+
+setup(
+    name="Products.ZMySQLDA",
+    version="3.0",
+    license="Zope Public License (ZPL) Version 1.0",
+    author="John Eikenberry",
+    author_email="jae@zhar.net",
+    url="http://sourceforge.net/projects/mysql-python",
+    description="MySQL Zope2 adapter.",
+    long_description=("MySQL Database Adapter for Zope 2. Extensively "
+        "reworked for stability and compatibility with versions 2.8+ and 
+        "modern MySQL versions. New features from auto-creating database "
+        "to limited Unicode support."),
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['Products'],
+    zip_safe=False,
+    install_requires = "MySQL-python >= 1.2.1",
+    )
+
